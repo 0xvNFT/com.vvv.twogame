2f6822cba15885afee596787c2575dd16303cc0a
Jayson <kanekiken101017@gmail.com>
2023-08-15 12:48:33 +0800

    initial commit, menu & structure

---
+++ app/src/main/java/com/vvv/twogame/game2/WhackAMoleActivity.java
@@ -0,0 +1,15 @@
+package com.vvv.twogame.game2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vvv.twogame.R;
+
+public class WhackAMoleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whackamole);
+    }
+}
